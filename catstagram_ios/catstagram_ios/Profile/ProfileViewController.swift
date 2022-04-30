88fd991e79a0e7c4ee7842a6801bb1b6406484af
Euna Ahn <eunaahn2001@icloud.com>
2022-05-01 00:05:26 +0900

    profile collection view

--- catstagram_ios/catstagram_ios/Profile/ProfileViewController.swift
+++ catstagram_ios/catstagram_ios/Profile/ProfileViewController.swift
@@ -22,9 +22,11 @@ class ProfileViewController: UIViewController {
     
     //MARK: -Helpers
     private func setupCollectionView(){
+        //delegate 연결
         profileCollectionView.delegate = self
         profileCollectionView.dataSource = self
         
+        //cell 등록
         profileCollectionView.register(
             UINib(
                 nibName: "ProfileCollectionViewCell",
@@ -41,8 +43,17 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCollectionViewCell.identifier, for: indexPath) as? ProfileCollectionViewCell else{
+//            return UICollectionViewCell()
+            fatalError("샐 타입 캐스팅 실패...")
+        }
+        return cell
     }
     
-    
+}
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: CGFloat(159))
+    }
 }
